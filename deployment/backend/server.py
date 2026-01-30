4f1e896c9e0108746889ef5e4bdbc0d4b68caaa7
Oualidelboukh <oelboukhrissi@gmail.com>
2026-01-30 10:27:20 +0100

    Add main entry point for Uvicorn server
    Added main entry point to run the server with Uvicorn.

--- deployment/backend/server.py
+++ deployment/backend/server.py
@@ -95,3 +95,6 @@ logger = logging.getLogger(__name__)
 @app.on_event("shutdown")
 async def shutdown_db_client():
     client.close()
+    if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("server:app", host="0.0.0.0", port=8080)
